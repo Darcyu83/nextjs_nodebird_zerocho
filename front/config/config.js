c077d08a16050e3a4bab616d7e7005371820c711
Darcyu83 <darcy.u83@gmail.com>
2023-01-28 16:51:37 +0900

    aws elastic IPs 적용 : ip고정

--- front/config/config.js
+++ front/config/config.js
@@ -1,5 +1,5 @@
 export const backUrl =
   process.env.NODE_ENV === "production"
     ? // ? "http://api.nodebird.com"
-      "http://3.34.135.56"
+      "http://http://3.36.241.163/"
     : "http://localhost:5000";
